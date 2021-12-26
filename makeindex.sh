3e27697bcd3c9923160b218680f9028ba2bb48be
Toni Magni <afm@ubuntu-p1>
2021-12-27 00:54:11 +0100

    More index improvements.

--- makeindex.sh
+++ makeindex.sh
@@ -12,9 +12,9 @@ printf "
   <h1>Africa Journal</title>
   <table>\n" >$INDEX
 
-for file in $(find -name "day*.html"); do
+for file in $(ls -1 day*.html | sort); do
     {
-        printf "    <tr><td>"
+        printf "    <tr><td class=\"day_index\">"
         printf "<a href=\"%s\">%s</a>" "$file" "$(basename $file .html)"
         printf "</td></tr>\n"
     } >>$INDEX
